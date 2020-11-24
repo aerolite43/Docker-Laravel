3c27c7bc6c1f30173f45c46b20a0948107aaf637
Adrian <adrian_baguio43@yahoo.com>
2020-11-23 21:10:15 -0500

    Add laravelcollective to build forms. Add contact blade forms using laravelcollective.

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
     <h1>Contact</h1>
+    {!! Form::open(['url' => 'contact/submit']) !!}
+        <div class="form-group">
+<?php 
+            echo Form::label('name', 'Name'); 
+            echo Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Enter name']);
+?>
+        </div>
+        <div class="form-group">
+<?php
+            echo Form::label('email', 'E-Mail Address');
+            echo Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Enter email']);
+?>
+        </div>
+        <div class="form-group">
+<?php
+            echo Form::label('message', 'message');
+            echo Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Enter Message']);
+?>
+        </div>
+        <div>
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+        </div>
+    {!! Form::close() !!}
 @endsection
